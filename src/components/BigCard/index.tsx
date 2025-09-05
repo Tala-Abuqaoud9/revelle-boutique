c54ecd8dd1b96d2f860fb26a1690cf9021fe8a25
Tala-Abuqaoud <tala.m.abuqaoud@gmail.com>
2025-09-05 13:52:35 +0300

    first commit

--- src/components/BigCard/index.tsx
+++ src/components/BigCard/index.tsx
@@ -19,33 +19,14 @@ export default function Card({ imageSrc, title , className}: Props){
   const [selectedColor , setSelectedColor] = useState<string | null>(null);
 return(
 <div className="relative w-100 bg-white overflow-hidden group">
-  <div className="relative h-96 w-[350px]">
+  <div className="relative h-96 w-[710px]">
     <Link href="#">
       <Image src={imageSrc} alt={title} fill className="object-cover" />
     </Link>
 
-    <div className="hidden group-hover:flex absolute inset-0 bg-black bg-opacity-30 transition-opacity duration-300">
-      <div className="absolute top-4 right-4 z-10">
-      <Heart
-        className={`w-6 h-6 ${isFavorite ? "text-red-500" : "text-white"}`}
-        onClick={() => setIsFavorite(!isFavorite)}
-      />
-  </div>
+    <div className="hidden group-hover:flex absolute inset-0 bg-black bg-opacity-5 transition-opacity duration-300">
    <div className="absolute bottom-4 left-4 z-10 text-white">
-    <h2 className="text-lg font-bold">Hijabi Outfit</h2>
-    <div className="flex gap-2 mt-2">
-      {colors.map((color) => (
-        <Button
-          key={color}
-          label=""
-          className={`w-5 h-5 rounded-full border-2 ${
-            selectedColor === color ? "border-black" : "border-white"
-          }`}
-          style={{ backgroundColor: color }}
-          onClick={() => setSelectedColor(color)}
-        />
-      ))}
-    </div>
+    <h2 className="text-lg font-bold">Bag1</h2>
   </div>
     </div>
   </div>
--- src/components/Features/newInSection/index.tsx
+++ src/components/Features/newInSection/index.tsx
@@ -1,5 +1,21 @@
-" use client"
+" use client";
+import BigCard from "@/components/BigCard";
+
+type Props = {
+  imageSrc: string;
+  description: string;
+};
 
 export default function NewInSection() {
-  return <div>New In Section</div>;
+  const cards = [
+    { imageSrc: "/images/bag1.avif", description: "Elegant Handbag" },
+    { imageSrc: "/images/bag2.avif", description: "Stylish Backpack" },
+  ];
+  return (
+    <div className="flex flex-wrap justify-between gap-1 w-full">
+      {cards.map((card, index) => (
+          <BigCard key={index} imageSrc={card.imageSrc} title={card.description} />
+      ))}
+    </div>
+  );
 }
